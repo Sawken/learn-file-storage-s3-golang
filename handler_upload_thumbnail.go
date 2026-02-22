4bd724649b05d480e5bfa4ce8ae70c5e4b987dc6
Sawken <5907455+Sawken@users.noreply.github.com>
2026-02-22 15:33:44 +0100

    initial commit

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,55 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusUnprocessableEntity, err.Error(), err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusUnprocessableEntity, err.Error(), err)
+		return
+	}
+
+	videoType := header.Header.Get("Content-Type")
+	fmt.Printf("MEDIA_TYPE: %s\n", videoType)
+	var fileData []byte
+	fileData, err = io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusUnprocessableEntity, err.Error(), err)
+		return
+	}
+	videoIdUUID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusUnprocessableEntity, err.Error(), err)
+		return
+	}
+	videoRow, err := cfg.db.GetVideo(videoIdUUID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error(), err)
+		return
+	}
+	if videoRow.UserID.String() != userID.String() {
+		respondWithError(w, http.StatusUnauthorized, err.Error(), err)
+		return
+	}
+	fmt.Printf("FILE_DATA: %+v", fileData)
+	videoThumbnails[videoRow.ID] = thumbnail{
+		data:      fileData,
+		mediaType: videoType,
+	}
+	*videoRow.ThumbnailURL = "/api/thumbnail/" + videoIDString
+	err = cfg.db.UpdateVideo(videoRow)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error(), err)
+		return
+	}
+	fmt.Printf("THUMBNAIL: %+v\n", videoThumbnails[videoRow.ID])
+	respondWithJSON(w, http.StatusOK, &videoRow)
 }
